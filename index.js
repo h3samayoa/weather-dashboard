1e8cb2fee502dc0329d53431ee8503611a6a548c
h3samayoa <h3samayoa@gmail.com>
2021-10-11 00:17:49 -0700

    fixed forecast bug

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ $(document).ready(function() {
     const uvi = $('#uvi');
     const srchHistory = $('#history');
     const apiKey = '81c2df482de1c0b8aacf94a4b39b0770';
-    const cityTest = 'London';
-    const currentDate = moment(new Date()).format("MM/DD/YYYY")
+    const currentDate = moment(new Date()).format("MM/DD/YYYY");
+    let history = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(history);
+    let forecastEl;
+    let forecastTempIcon;
+    let forecastTempVal;
+    let forecastHumidVal;
 
 
     function tempConverter(temp) {
@@ -50,8 +55,8 @@ $(document).ready(function() {
                 humid.html('Humidity: ' + data.main.humidity + "%");
                 windSpd.html('Wind Speed: ' + data.wind.speed + " MPH")
 
-                var lat = data.coord.lat;
-                var long = data.coord.lon;
+                lat = data.coord.lat;
+                long = data.coord.lon;
 
                 forecast(lat, long);
             }
@@ -84,21 +89,21 @@ $(document).ready(function() {
                 uvi.append(uviBadge)
                 const forecastCards = $('[id=forecast]')
                 for (let i = 0; i < forecastCards.length; i++) {
-                    let forecastEl = $('<p></p>');
+                    forecastEl = $(`<p id="${i}"></p>`);
                     forecastEl.attr("class", "mt-3 mb-0 date");
                     forecastEl.html(dates[i]);
                     forecastCards[i].append(forecastEl[0]);
-                    const forecastTempIcon = $('<img>');
+                    forecastTempIcon = $(`<img id="img${i}">`);
                     let forecastIcon = data.daily[i].weather[0].icon;
                     forecastTempIcon.attr("src", `https://openweathermap.org/img/wn/${forecastIcon}@2x.png`);
                     console.log(forecastTempIcon[0]);
                     forecastCards[i].append(forecastTempIcon[0]);
-                    let forecastTempVal = $('<p></p>');
+                    forecastTempVal = $(`<p id="temp${i}"></p>`);
                     forecastTempVal.html('Temp: ' + tempConverter(data.daily[i].temp.day) + " &#176F");
                     forecastCards[i].append(forecastTempVal[0]);
-                    let forecastHumidVal = $('<p></p>');
+                    forecastHumidVal = $(`<p id="humid${i}"></p>`);
                     forecastHumidVal.html('Humidity: ' + data.daily[i].humidity + "%");
-                    forecastCards[i].append(forecastHumidVal[0])
+                    forecastCards[i].append(forecastHumidVal[0]);
                 }
             }
         }
@@ -106,6 +111,54 @@ $(document).ready(function() {
         xhttp.send();
     }
 
-    forecast();
-    searchCity(cityTest);
+    searchBtn.click(function() {
+        const city = inputCity.val();
+        console.log(city)
+        searchCity(city);
+        for (let i = 0; i < 5; i++) {
+            let forecastElRem = $(`#${i}`)
+            forecastElRem.remove();
+            let iconRem = $(`#img${i}`);
+            iconRem.remove();
+            let tempRem = $(`#temp${i}`)
+            tempRem.remove();
+            let humidRem = $(`#humid${i}`)
+            humidRem.remove();
+        }
+        history.push(city);
+        localStorage.setItem("search", JSON.stringify(history));
+        displayHistory();
+        if(history.length >= 10) {
+            history.splice(0, 1)
+        }
+    })
+    
+
+    function displayHistory() {
+        srchHistory.html("");
+        for (let i = 0; i < history.length; i++) {
+            const historyEL = $('<input></input>');
+
+            historyEL.attr("type", "text");
+            historyEL.attr("readonly", true);
+            historyEL.attr("class", "form-control d-block bg-white");
+            historyEL.attr("value", history[i]);
+            historyEL.click(function() {
+                searchCity(historyEL.val());
+                if(history.length >= 10) {
+                    history.splice(0, 1)
+                }
+            })
+            srchHistory.append(historyEL);
+        }
+    }
+
+    displayHistory();
+    if (history.length > 0) {
+        searchCity(history[history.length - 1]);
+    }
+    if(history.length >= 10) {
+        history.splice(0, 1)
+    }
+
 });
