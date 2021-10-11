5a1f39dc41782e7b0abc8743a4d95770c1db7398
h3samayoa <h3samayoa@gmail.com>
2021-10-10 19:35:29 -0700

    get uv index

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ $(document).ready(function() {
     const windSpd = $('#windSpd');
     const uvi = $('#uvi');
     const srchHistory = $('#history');
-    const cityTest = 'London';
     const apiKey = '81c2df482de1c0b8aacf94a4b39b0770';
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityTest}&appid=${apiKey}`;
+    const cityTest = 'London';
 
 
     function tempConverter(temp) {
@@ -18,6 +17,7 @@ $(document).ready(function() {
     }
 
     function searchCity(city) {
+        let searchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
@@ -36,11 +36,44 @@ $(document).ready(function() {
                 humid.html('Humidity: ' + data.main.humidity + "%");
                 windSpd.html('Wind Speed: ' + data.wind.speed + " MPH")
 
+                var lat = data.coord.lat;
+                var long = data.coord.lon;
+
+                forecast(lat, long);
             }
         }
-        xhttp.open("GET", apiUrl, true);
+        xhttp.open("GET", searchUrl, true);
         xhttp.send();
     }
 
+    function forecast(lat, long) {
+        console.log(lat, long);
+        let uvURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,minutely&appid=${apiKey}`
+        console.log(uvURL)
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var data = JSON.parse(this.responseText);
+                console.log(data)
+                
+                let uviBadge =  $("<span></span>")
+                let uviVal = data.current.uvi;
+                if (uviVal >= 0 && uviVal <= 2.99) {
+                    uviBadge.attr("class", "badge badge-success");
+                } else if (uviVal >= 3 && uviVal <= 7.99) {
+                    uviBadge.attr("class", "badge badge-warning");
+                } else if (uviVal >= 8) {
+                    uviBadge.attr("class", "badge badge-danger");
+                }
+                uviBadge.html(uviVal)
+                uvi.html('UV Index: ');
+                uvi.append(uviBadge)
+            }
+        }
+        xhttp.open("GET", uvURL, true);
+        xhttp.send();
+    }
+
+    forecast();
     searchCity(cityTest);
 });
